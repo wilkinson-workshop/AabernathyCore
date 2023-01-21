df755dd6845802482351cdc94fde7c5bea02afa7
WilkinsonK <keenanwilkinson@outlook.com>
2023-01-21 14:15:20 -0600

    initial commit

---
+++ src/main/java/org/ww/adt/aabernathycore/AabernathyCore.java
@@ -0,0 +1,17 @@
+package org.ww.adt.aabernathycore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AabernathyCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
