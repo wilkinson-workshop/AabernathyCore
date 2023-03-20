b17fdf3139777260bdec62af236ed0c8ebe08f38
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2023-03-19 21:44:25 -0500

    implement new API

--- src/main/java/org/ww/adt/spigot/Plugin.java
+++ src/main/java/org/ww/adt/spigot/Plugin.java
@@ -1,18 +1,35 @@
 package org.ww.adt.spigot;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.ww.adt.AabenernathyI;
+import org.ww.adt.api.Aabernathy;
+
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public final class Plugin extends JavaPlugin
 {
+    /**
+     * Core implementation API of Aabernathy plugin.
+     */
+    private AabenernathyI aabenernathy;
+
     @Override
     public void onEnable()
     {
-
+        try {
+            aabenernathy = new Aabernathy(this);
+            aabenernathy.start();
+        } catch (IOException error)
+        {
+            getLogger().warning("Failed to load API");
+            getLogger().warning(error.toString());
+        }
     }
 
     @Override
     public void onDisable()
     {
-
+        aabenernathy.stop();
     }
 }
