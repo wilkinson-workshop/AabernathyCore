f9629e9a74531462f8417ede5df2644c97593c8b
WilkinsonK <keenanwilkinson@outlook.com>
2023-03-27 21:27:27 -0500

    playing with deps

--- src/main/java/org/ww/adt/Aabernathy.java
+++ src/main/java/org/ww/adt/Aabernathy.java
@@ -3,6 +3,7 @@ package org.ww.adt;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
+import org.ww.adt.data.Engine;
 import org.ww.adt.events.PlayerEventListener;
 
 import java.io.IOException;
@@ -31,7 +32,9 @@ public class Aabernathy implements AabernathyAPI {
 
         // Initialize component API.
         AabernathyComponent.setApiInstance(this);
-
+        
+        new Engine();
+        
         // Set event listeners.
         new PlayerEventListener();
     }
